14c66aa1925179c93f828525c78901aea0d9e5ce
Martina <martina.gabrielli@yahoo.it>
2024-01-09 19:39:59 +0000

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,29 @@ import './App.css'
 
 function App() {
   const [catFact, setCatFact] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const getCatFact = () => {
-    fetch("https://catfact.ninja/fact").then(res => res.json()).then((data) => setCatFact(data.fact))
+    setIsLoading(true)
+    fetch("https://catfact.ninja/fact")
+    .then(res => res.json())
+    .then((data) =>  {
+      setCatFact(data.fact)
+      setIsLoading(false)
+    })
+    .catch(() => {
+      setIsLoading(true)
+    })
   }
 
   useEffect(()=> {
     getCatFact()
   }, [])
-  
+
   return (
     <>
-      <button onClick={getCatFact}>Get cat fact</button>
-      <p>{catFact}</p>
+      <button onClick={getCatFact} disabled={isLoading}>Get cat fact</button>
+      {isLoading ? <p>Loding...</p> : <p>{catFact}</p>}
     </>
   )
 }
