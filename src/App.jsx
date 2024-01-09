a05b1b6304c1bb6f4121b222666b909dd00d9727
Martina <martina.gabrielli@yahoo.it>
2024-01-09 19:25:00 +0000

    use fetch to get cat fact data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [catFact, setCatFact] = useState('');
+  useEffect(()=> {
+    fetch("https://catfact.ninja/fact").then(res => res.json()).then((data) => setCatFact(data.fact))
+  }, [])
   return (
     <>
-      <p>The world is your oyster</p>
+      <p>{catFact}</p>
     </>
   )
 }
