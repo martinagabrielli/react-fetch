44486a4e03080290aa843e26761772e286f9d697
Martina <martina.gabrielli@yahoo.it>
2024-01-09 19:34:47 +0000

    show cat fact when clicking on button

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import './App.css'
 
 function App() {
   const [catFact, setCatFact] = useState('');
-  useEffect(()=> {
+
+  const getCatFact = () => {
     fetch("https://catfact.ninja/fact").then(res => res.json()).then((data) => setCatFact(data.fact))
+  }
+
+  useEffect(()=> {
+    getCatFact()
   }, [])
+  
   return (
     <>
+      <button onClick={getCatFact}>Get cat fact</button>
       <p>{catFact}</p>
     </>
   )
